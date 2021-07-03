6ede8194fd3d0448cac55539fbbe7aa7a98c2995
Sze Tyng Lee <lee.szetyng@gmail.com>
2021-07-04 00:33:32 +0800

    06808c1

---
+++ _nuxt/static/1625329944/blog/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/blog", {data:[{articles:[{title:"First Post!",description:"A rough introduction to the blog section. Welcome!",author:null,img:"test.jpg",slug:"first-post"}]}],fetch:[],mutations:void 0});
